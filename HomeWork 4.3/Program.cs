4ebe9fc3023b7fc5a1e73aa56e9bed6c2caeab9d
ievali <ieval@LAPTOP-SQ4F164V>
2021-10-25 16:32:20 +0300

    New updates

---
+++ HomeWork 4.3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace HomeWork_4._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Array
+            
+   
+            int[] testArray = { 1, 2, 3, 4, 5};
+            int sum = 0;
+            for (int i = 0; i < testArray.Length; i++)
+                sum += testArray[i];
+            Console.WriteLine($"Sum = {sum}");
+            Console.ReadLine();
+
+
+                string[] stringArray = { "name", "surname" };
+            double[] doubleArray = { 2.3, 22, 3.4 };
+            #endregion
+
+            #region Loop
+            for (int i = 0; i < testArray.Length; i++)
+            { 
+                Console.WriteLine($"For loop iteration number: {testArray[i]}");
+                
+                //break;
+            }
+
+            string b = stringArray[1];
+            Console.WriteLine($"The second element is: {b}" );
+
+            #endregion
+
+            #region ClassWork1
+            int[,] array = { { 11, 23, 44 }, { 34, 422, 332 } };
+            for (int i = 0; i <2; i++)
+            {
+                for (int j=0; j <3; j++)
+                {
+                    Console.WriteLine(array[i,j]);
+                }
+            }
+
+            #endregion
+            int[] arr1 = new int[5];
+            for (int i =0; i <5; i++)
+            {
+                arr1[i] = i++;
+                Console.WriteLine(arr1[i]);
+            }
+
+            static void FindMaxNumber()
+            {
+                int[] intArray = { 123123213, 231223, 2222, 34334 };
+                var biggestVal = intArray.Max();
+                var smallestVal = intArray.Min();
+
+            }
+        }
+    }
+}
---
+++ Homework 4.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Homework_4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 12, 223, 3343, 45345, 23423, 23, 1112, 456, 33, 1 };
+            int[] array2 = new int[10];
+
+            array1.CopyTo(array2, 0);
+
+            foreach (int item in array2)
+            {
+                Console.Write($"{item}, ");
+            }
+
+            
+            Console.ReadLine();
+        }
+    }
+}
