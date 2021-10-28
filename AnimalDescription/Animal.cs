1e7519aa3aa3d2783e2a44131fd1f7e2f38edb51
ievali <ieval@LAPTOP-SQ4F164V>
2021-10-28 21:11:12 +0300

    New Animal task added

---
+++ AnimalDescription/Animal.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalDescription
+{
+    class Animal
+    {
+        private string EyeColour;
+        private string SkinColour;
+        private string Weight;
+        private string Height;
+        private string Age;
+
+        public string eyeColour
+        {
+            get { return this.EyeColour; }
+            set { this.EyeColour = value; }
+        }
+
+        public string skinColour
+        {
+            get { return this.SkinColour; }
+            set { this.SkinColour = value; }
+        }
+
+        public string weight
+        {
+            get { return this.Weight; }
+            set { this.Weight = value; }
+        }
+
+        public string height
+        {
+            get { return this.Height; }
+            set { this.Height = value; }
+        }
+
+        public string age
+        {
+            get { return this.Age; }
+            set { this.Age = value; }
+        }
+
+
+    }
+}
---
+++ AnimalDescription/Description.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalDescription
+{
+    public class Description
+    {
+        private static void Main(string[] args)
+        {
+            var animal = new Animal()
+            {
+                eyeColour = "Blue",
+                skinColour = "Black",
+                weight = "Heavy",
+                height = "Large",
+                age = "Old"
+            };
+
+            Console.WriteLine($"The eye color of the animal is: {animal.eyeColour}.");
+            Console.WriteLine($"The skin color of the animal is: {animal.skinColour}.");
+            Console.WriteLine($"The weight of the animal is: {animal.weight}.");
+            Console.WriteLine($"The height of the animal is: {animal.height}.");
+            Console.WriteLine($"The age of the animal is: {animal.age}.");
+            Console.ReadLine();
+
+        }
+    }
+}
